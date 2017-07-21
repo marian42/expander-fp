1facdb4a0c708e83b02cf5885962384fb5e606a8
Marco Pennekamp <marco.pennekamp96@gmail.com>
2017-07-21 23:37:40 +0200

    Create proper Table type via newtype instead of a type alias.

--- expander-system/src/Philosophers.hs
+++ expander-system/src/Philosophers.hs
@@ -7,27 +7,28 @@ import Data.List
 import TransitionSystem
 
 -- Represents the eating state of each philosopher as a bit in an integer.
-type Table = Int
+newtype Table = Table Int
+    deriving (Show, Eq, Ord)
 
 -- The table ID of a philosopher.
 type PhilosopherId = Int
 
 isEating :: Table -> PhilosopherId -> Bool
-isEating table id = ((table `div` (2 ^ id)) `mod` 2) == 1
+isEating (Table table) id = ((table `div` (2 ^ id)) `mod` 2) == 1
 
 setEating :: Table -> PhilosopherId -> Bool -> Table
-setEating table id shouldEat = updateTable shouldEat (isEating table id)
+setEating (Table table) id shouldEat = Table $ updateTable shouldEat (isEating (Table table) id)
     where updateTable True False = table + (2 ^ id)
           updateTable False True = table - (2 ^ id)
           updateTable _ _ = table
 
-instance SystemNode Int where
-    type UserData Int = Int
-    type Start Int = ()
+instance SystemNode Table where
+    type UserData Table = Int
+    type Start Table = ()
     toString table count = "(" ++ inner ++ ")"
         where inner = intercalate ", " (map label [0..(count - 1)])
               label id = if isEating table id then "e" else "t"
-    first _ = 0
+    first _ = Table 0
     transitions table count = concat (map tryFlip [0..(count - 1)]) where
         tryFlip id =
             if isEating table id
